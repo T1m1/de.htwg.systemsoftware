209b5e2b9b66f37eb88be78651dec978507aeb37
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2014-11-10 17:49:03 +0100

    new way (old way)

---
+++ v2/initramfs/init.sh
@@ -0,0 +1,32 @@
+#!/bin/busybox sh 
+
+/bin/busybox --install -s /bin/
+
+clear
+
+# Say Hello
+echo '######################'
+echo 'Starting system'
+echo '######################'
+
+#mount
+echo '--> Mounting...'
+mount -t proc proc /proc
+mount -t sysfs none /sys
+mount -t tmpfs none /tmp
+echo '--> done'
+
+
+mdev -s
+
+
+echo 0 > /proc/sys/kernel/printk
+
+#start init program and display system infos
+echo '--> start binary'
+./bin/start
+
+
+#end
+echo '--> Starting shell'
+setsid cttyhack sh
