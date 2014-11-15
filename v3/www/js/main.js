bb91d00dc7e6e6c973ef00958f9c04a85e415114
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-15 15:37:16 +0100

    refactor js for web

--- v3/www/js/main.js
+++ v3/www/js/main.js
@@ -1,1 +1,55 @@
-controller.run();
+$(document).ready(function () {
+	
+	// hash array with all functions
+	var hash = {
+		'#cpu': 'cgi-bin/cpu-info.sh',
+		'#osinfo': 'cgi-bin/os-info.sh',
+		'#devices': 'cgi-bin/device-info.sh',
+		'#mem': 'cgi-bin/mem-info.sh',
+		'#network': 'cgi-bin/net-info.sh',
+		'#deleteTmp': 'cgi-bin/delete-tmp.sh',
+		'#uptime': 'cgi-bin/uptime.sh'
+	};
+	
+	// click event on buttons (a - tag)
+	$('a').on('click', function (event) {
+		var current = event.currentTarget.hash;
+		if(!current || !hash[current]) {
+			event.preventDefault();
+			return;
+		}
+		
+		$.ajax({
+			url: hash[current],
+			dataType: "html",
+			success: function(data) {
+				replaceHtml(data, '#output');
+			}
+		});
+	});
+	
+	
+	// show new values
+	var replaceHtml = function(data, elemId) {
+		$(elemId).html(data);
+	};
+	
+	var request = function(key) {
+		$.ajax({
+			url: hash[key],
+			dataType: "html",
+			success: function(data) {
+				replaceHtml(data, key);
+			}
+		});
+	};
+	
+	(function iniUptime() {
+		request('#uptime');
+		request('#osinfo');
+		
+		setInterval(request('#uptime'), 10000);
+		setInterval(request('#osinfo'), 10000);
+	})();
+	
+});
