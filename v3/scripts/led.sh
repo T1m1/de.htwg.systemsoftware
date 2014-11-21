d0b31d981ce48a86fd8b56b51cc28dc1aa75f59c
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-21 08:50:34 +0100

    refactor led test script

--- v3/scripts/led.sh
+++ v3/scripts/led.sh
@@ -3,14 +3,16 @@
 GPIO=18
 ON=1
 OFF=0
-LED=/sys/class/gpio/gpio$GPIO/values
+LED=/sys/class/gpio/gpio$GPIO/value
+
+# pin bei abbrechen des scripts freigeben
+trap "echo \"$GPIO\" >/sys/class/gpio/unexport" EXIT
 
 while true
 do
-	echo ON > $LED
+	echo "$ON" > $LED
 	sleep 1
-
-	echo OFF > $LED
+	echo "$OFF" > $LED
 	sleep 1
 done
 
