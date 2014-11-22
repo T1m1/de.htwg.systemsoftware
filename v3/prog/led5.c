31e300efb78db80a3f49bb820b64f997ee92f6e8
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2014-11-22 22:53:47 +0100

    everything ready for reading values and writing values

--- v3/prog/led5.c
+++ v3/prog/led5.c
@@ -2,71 +2,100 @@
 #include <stdio.h>
 #include <time.h>
 #include <signal.h>
+#include <pthread.h>
 
 // file handle in linux
-#include <fcnt1.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+//#include <fcnt1.h>
+//#include <sys/stat.h>
+//#include <sys/types.h>
 
 struct thread_info
 {
 	int *button_pressed;
-	struct timespec *sleep_time;
-}
+	struct timespec sleep_time;
+};
 
 void *blink(void *thread_info);
 void sigHandler(int);
 
-void main(void)
+int
+main(void)
 {
+	
+	int value;
+	int status;
 	pthread_t blink_thread;
+	struct thread_info *thread_struct = NULL;
+	struct timespec sleep_time;
+	
 	signal(SIGINT, sigHandler);
-	struct thread_info *thread_struct;
 	
 	// init thread_struct
+	sleep_time.tv_sec = 1;
+	sleep_time.tv_nsec = 200000000;
+	status = 1;
+	
+	thread_struct = malloc(sizeof (struct thread_info));
+	//TODO fehlerbehandlung
+	thread_struct->sleep_time = sleep_time;
+	thread_struct->button_pressed = &status;	
+	
 	
 	// start thread
-	if (pthread_create(&blink_thread, NULL, blink, &thread_struct) != 0) 
+	if (pthread_create(&blink_thread, NULL, blink, thread_struct) != 0) 
 	{
 		fprintf(stderr, "Failed to create thread. Terminating.\n");
 		return(-1);
 	}
 	
+	
+	value = 1;
 	for (;;)
 	{
 		// read value from switch
 		
-		if (value == '1')
+		if (value ==1)
 		{				
-			thread_struct->b = 1;
+			status = 1;
+			sleep(4);
+			printf("Setting status on 0\n");
+			status = 0;
+			sleep(4);
+			
 		}
 		else
 		{
-			thread_struct->b = 0;
+			status = 0;
 		}
 		
+		thread_struct->button_pressed = &status;	
 	}
+	
 	
 }
 
 void *
 blink (void *thread_info)
 {
-	int s, b;
-	struct timespec *remain;
-	struct timespec *request = thread_info->timespec;
 	
-	b = thread_info->button_pressed;
+	int s;
+	struct thread_info *t = (struct thread_info *) thread_info;
+	
+	struct timespec request = t->sleep_time;
+	int *b = t->button_pressed;
+	
 	for (;;)
 	{
 		// blink only when button was pressed
-		if (b == 1)
+		if (*b == 1)
 		{
 			// led on
+			printf("AN\n");
 		
-			s = clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &request, &remain);
+			clock_nanosleep(CLOCK_REALTIME, 0, &request, NULL);
 		
 			// led off
+			printf("AUS\n");
 		}
 	}
 } 
@@ -74,9 +103,10 @@ blink (void *thread_info)
 void 
 sigHandler(int sig)
 {
-	printf("End led blinking");
+	printf("\nEnd led blinking\n");
 	// led off
 	
+	//TODO free stuff
 	exit(0);
 		
 }
