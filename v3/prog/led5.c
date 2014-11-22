a565c7f9326f0245e18477600d1cef3d0261ccd1
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2014-11-22 17:53:41 +0100

    early implementation of led5

---
+++ v3/prog/led5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+// file handle in linux
+#include <fcnt1.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void main(void)
+{
+	int threadStarted;
+		
+		
+	threadStarted = 0;
+	
+	while (0) 
+	{
+		// read value from switch
+		
+		if (value == '1')
+		{
+			if (threadStarted != 1)
+			{
+				threadStarted = 1;
+				// start thread
+			}
+		}
+		else
+		{
+			// stop thread
+			threadStarted = 0;
+		}
+		
+	}
+	
+	//TODO: how to end the programm
+	
+}
