952a090238a006b512e37966df1d3c1a6306a38e
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2014-11-23 18:08:49 +0100

    remove of unused functions

--- v3/prog/switch.c
+++ v3/prog/switch.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -8,11 +9,7 @@
 
 #include "gpio.h"
 
-#define BUFFER_MAX 3
-#define DIRECTION_MAX 35
 #define GPIO_PIN 17
-#define BUF 10
-#define VALUE_MAX 30
 
 void sigHandler(int);
 int done;
@@ -67,96 +64,5 @@ sigHandler(int not_used)
 {
 	printf("Ending Programm\n");
 	done = 1;
-}
-
-int GPIOExport(int gpio)
-{
-	int status = 0;
-	int fd;
-	char buffer[BUFFER_MAX];
-	ssize_t bytes_written;
-	
-	fd = open("/sys/class/gpio/export", O_WRONLY);
-	// test fd = open("/home/tiruprec/gpio/export", O_WRONLY);	
-	if (-1 == fd)
-	{
-		fprintf(stderr, "Failes to open exort for writing!\n");
-		return(-1);
-	}
-	
-	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", gpio);
-	write(fd, buffer, bytes_written);
-	close(fd);
-	
-	return status;
-}
-
-int GPIODirection(int gpio) {
-	
-	int status = 0;
-	int fd;
-	char path[DIRECTION_MAX];
-
-	//snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", gpio);
-	snprintf(path, DIRECTION_MAX, "/home/tiruprec/gpio%d/direction", gpio);
-	fd = open(path, O_WRONLY);
-	if (-1 == fd)
-	{
-		fprintf(stderr, "Failed to open gpio dirction for writing!\n");
-		return(-1);
-	}
-	
-	if(-1 == write(fd, "in", 2))
-	{
-		fprintf(stderr, "Failed to set direction!\n");
-		return(-1);
-	}
-	
-	close(fd);
-	return status;
-}
-
-int GPIOUnexport(int gpio)
-{
-	int fd;
-	char buffer[BUFFER_MAX];
-	ssize_t bytes_written;
-	
-	fd = open("/sys/class/gpio/unexport", O_WRONLY);
-	if(-1 == fd)
-	{
-		fprintf(stderr, "Failed to open unexport for writing!\n");
-		return(-1);
-	}
-	
-	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", gpio);
-	write(fd, buffer, bytes_written);
-	close(fd);
-	return(0);
-}
-
-int GPIORead(int gpio)
-{
-	char s_value[3];
-	char path[VALUE_MAX];
-	int fd;
-
-	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", gpio);
-	// test snprintf(path, VALUE_MAX, "/home/tiruprec/gpio%d/value2", gpio);
-	fd = open(path, O_RDONLY);
-	if (-1 == fd) 
-	{
-		fprintf(stderr, "Failed to open gpio value for reding!\n");
-		return(-1);
-	}
-
-	if (-1 == read(fd, s_value, 3))
-	{
-		fprintf(stderr, "Failed to read value!\n");
-		return(-1);
-	}
-
-	close(fd);
-	return (atoi(s_value));;
 }
 
