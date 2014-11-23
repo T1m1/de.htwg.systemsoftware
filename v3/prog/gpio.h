f195bb969cb00c8fefa7a91e62b0dc342f99d1f3
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2014-11-23 16:28:58 +0100

    Lib added for handling GPIO

---
+++ v3/prog/gpio.h
@@ -0,0 +1,13 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+int gpio_export(unsigned int gpio);
+int gpio_unexport(unsigned int pgio);
+int gpio_set_dir(unsigned int gpio, unsigned int out_flag);
+int gpio_set_value(unsigned int gpio, unsigned int value);
+int gpio_get_value(unsigned int gpio, unsigned int *value);
+int gpio_set_edge(unsigned int gpio, char *egde);
+int gpio_fd_open(unsigned int gpio);
+int gpio_fd_clos(int fd);
+
+#endif
