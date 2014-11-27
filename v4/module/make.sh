3a4f48b08f6346422a5c90610fc8bd2a9894ddb8
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-27 13:34:47 +0100

    test paths for issue

--- v4/module/make.sh
+++ v4/module/make.sh
@@ -4,8 +4,7 @@ LINUX_VERSION=linux-3.17.2
 
 ARCH=arm
 CROSS_COMPILE=/group/labworks/toolchain/buildroot.git/output/host/usr/bin/arm-buildroot-linux-uclibcgnueabihf-
-KDIR=~/syso_ws1415_14/buildroot/output/build/$LINUX_VERSION
+KDIR=/group/SYSO_WS1415_14/syso_ws1415_14/buildroot/output/build/$LINUX_VERSION
 
 
-
-make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE KDIR=$KDIR 
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE KDIR=$KDIR	PWD=$PWD
