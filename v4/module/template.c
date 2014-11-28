993790150c6b4198754c88e945b920d71cf3b3f5
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-28 13:42:23 +0100

    add register_chrdev function

--- v4/module/template.c
+++ v4/module/template.c
@@ -1,19 +1,28 @@
 #include <linux/init.h>
 #include <linux/module.h>
+/* prototypes */
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Timotheus Ruprecht and  Steffen Gorenflo");
 MODULE_DESCRIPTION("Modul Template");
 MODULE_SUPPORTED_DEVICE("none");
 
+static int major;
+static struct file_operations fobs;
 
 static int __init ModInit(void)
 {
 	printk(KERN_ALERT "HELLO, world\n");
+	/* on success register chdev returns 0 */
+	if((major=register_chrdev(0, "TestDriver", &fobs)) == 0) 
+	{
+		return major;
+	}	
 	return 0;
 }
 
-static void __exit ModExit(void)
+static void __exit ModExit(void) 
 {
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
