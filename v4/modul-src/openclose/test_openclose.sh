22109a3aaedfa12c5635195ced311f71c03d5657
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-29 18:28:59 +0100

    add test file for openclose

---
+++ v4/modul-src/openclose/test_openclose.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PROG_NAME=openclose
+
+# clear dmesg output
+dmesg -c > /dev/null
+
+# show modul informations
+echo "********** modul info **********"
+modinfo $PROG_NAME.ko
+
+# load modul
+insmod $PROG_NAME.ko
+
+# show kernel logs for loading (e.g. last 10 lines)
+echo "********** kernel logs for loading **********"
+dmesg -c
+
+# show information from /proc/devices
+echo "********** /proc/devices **********"
+cat /proc/devices
+
+# unload modul (without .ko)
+rmmod $PROG_NAME
+
+# show kernel logs for unload
+echo "********** kernel logs for unloading **********"
+dmesg -c
+
