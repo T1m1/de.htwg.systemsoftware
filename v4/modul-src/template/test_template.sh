e9602cb4546ab4f3f073b7f05520b6180fd3294e
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-29 18:25:56 +0100

    adjust template file

--- v4/modul-src/template/test_template.sh
+++ v4/modul-src/template/test_template.sh
@@ -4,20 +4,24 @@
 dmesg -c > /dev/null
 
 # show modul informations
+echo "**********modul info **********"
 modinfo template.ko
 
 # load modul
 insmod template.ko
 
 # show kernel logs for loading (e.g. last 10 lines)
+echo "********** kernel logs for loading **********"
 dmesg -c
 
 # show information from /proc/devices
+echo "********** /proc/devices **********"
 cat /proc/devices
 
 # unload modul (without .ko)
 rmmod template
 
 # show kernel logs for unload
+echo "********** kernel logs for unloading **********"
 dmesg -c
 
