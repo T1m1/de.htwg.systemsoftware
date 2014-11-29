404ebd03408910a13b80fba0fc21eadc981ba0fa
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-29 18:28:46 +0100

    small changes

--- v4/modul-src/template/test_template.sh
+++ v4/modul-src/template/test_template.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+PROG_NAME=template
+
 # clear dmesg output
 dmesg -c > /dev/null
 
 # show modul informations
-echo "**********modul info **********"
-modinfo template.ko
+echo "********** modul info **********"
+modinfo $PROG_NAME.ko
 
 # load modul
-insmod template.ko
+insmod $PROG_NAME.ko
 
 # show kernel logs for loading (e.g. last 10 lines)
 echo "********** kernel logs for loading **********"
@@ -19,7 +21,7 @@ echo "********** /proc/devices **********"
 cat /proc/devices
 
 # unload modul (without .ko)
-rmmod template
+rmmod $PROG_NAME
 
 # show kernel logs for unload
 echo "********** kernel logs for unloading **********"
