434cf6f2b3e748229efc2cac462455421515581e
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-11-30 10:43:01 +0100

    add number of threads as optarg

--- v4/prog/access.c
+++ v4/prog/access.c
@@ -2,21 +2,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define TRUE 1
+#define FALSE 0
+
+#define DEFAULT_NUMBER_OF_THREADS 5;
+
 /*
 	TODO
 	- paraleller zugriff -> Threading?
 	- option -o = (open Test)
 	- option -c = (close Test)
 	- option -t = (waint time)
-
+	- option -n = anzahl threads (default 5)
+	
 	Vorraussetzung:
 	- Treiber ist geladen
 */
 
+int numberOfThreads = DEFAULT_NUMBER_OF_THREADS;
 
 int main(int argc, char *argv[])
 {
+	/* parse options */
 	int opt;
+	int opentest, closetest, time = FALSE;	
 	while(-1 != (opt = getopt (argc, argv, "oct:"))) {
 		switch(opt){
 			case 'o':
@@ -28,7 +37,17 @@ int main(int argc, char *argv[])
 			case 't':
 				printf("waiting time: %s\n", optarg);
 				break;
+			case 'n':
+				printf("number of threads: %s\n", optarg);
+				numberOfThreads = atoi(optarg);
+				break;
 		}
 	}
+
+		
+
+
+
+
 	return 0;
 }
