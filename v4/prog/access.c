bebb712f4be6573b24e547847e64b7eab5b0ec90
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-12-02 15:51:47 +0100

    refactor

--- v4/prog/access.c
+++ v4/prog/access.c
@@ -3,19 +3,24 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define TRUE 1
 #define FALSE 0
 
 #define DEFAULT_NUMBER_OF_THREADS 5;
-#define DEFAULT_DURATION 500;
+#define DEFAULT_DURATION 1;
+#define DEFAULT_REPEAT 5;
 
 void *open_driver(void *info);
 
 /*
 	TODO
-	- paraleller zugriff -> Threading?
-	- option -o = (open Test)
+	- HELP text
+	- paraleller zugriff -> Threading? -> done
+	- option -o = (open Test) -> done
 	- option -c = (close Test)
 	- option -t = (waint time)
 	- option -n = anzahl threads (default 5)
@@ -34,11 +39,12 @@ int main(int argc, char *argv[])
 	int numberOfThreads = DEFAULT_NUMBER_OF_THREADS;
 	int opentest, closetest = FALSE;
 	duration = DEFAULT_DURATION;
+	repeat = DEFAULT_REPEAT;
 
 	/* threads */
 	pthread_t *threads;
 
-	while(-1 != (opt = getopt (argc, argv, "d:oct:r:"))) {
+	while(-1 != (opt = getopt (argc, argv, "d:oct:r:n:"))) {
 		switch(opt){
 			case 'o':
 				printf("open Test: %s\n", optarg);
@@ -69,7 +75,7 @@ int main(int argc, char *argv[])
 
 	/* check if path ommitted */
 	if(device == NULL){
-		printf("Usage: ./access [DEVICE_PATH] [OPTIONS]\n");
+		printf("Usage: ./access -d [DEVICE_PATH] [OPTIONS]\n");
 		return EXIT_FAILURE;
 	}
 		
@@ -81,18 +87,23 @@ int main(int argc, char *argv[])
 	/* open test */
 	int i;
 	if(opentest) {
+		printf("OpenTest:\n");
+		
 		for(i = 0; i < numberOfThreads; i++) {
+			printf("Start Thread %d\n", i);
 			pthread_create(&threads[i], NULL, open_driver, NULL);
 		}	
 
 
 		for(i = 0; i < numberOfThreads; i++) {
+			printf("Join Thread %d\n", i);
 			pthread_join(threads[i], NULL);
 		}
+		
 	}
 	
 	if(closetest) {
-
+		printf("Close Test:\n");
 	}
 	
 	free(threads);
@@ -102,31 +113,42 @@ int main(int argc, char *argv[])
 
 void *open_driver(void *info)
 {
-	int *openptr;
-	int ret, i;
+	int i, fd;
+	struct timespec sleep_time;
 
-	openptr = malloc(repeat * sizeof(int));
-	if (NULL == openptr) {
-		ret = EXIT_FAILURE;
-		pthread_exit(&ret);		
-	}
 	
-	/* open driver */
-	for(i = 0; i < repeat; i++) {
-		openptr[i] = open(device, O_RDONLY);
-	}
+	/* init time struct */
+	sleep_time.tv_sec = 1;
+	sleep_time.tv_nsec = 0;
+	
+	for(i = 0; i < repeat; i++ ){
+		printf("repeat %d", i);
 
-	/* wait -> sleep thread safe? */
-	sleep(duration);
+		printf("Try to open Driver...\n");
+		fd = open(device, O_RDONLY);
+		if (fd < 0) 
+		{
+			printf("error - open");
+		} else {
+			printf("Open Driver!\n");
+		}
+		
+		printf("Sleep %d Millisecond\n", duration);
+		/* wait -> sleep thread safe? !!!! sind sekunden!!! andere funktion*/
+		clock_nanosleep(CLOCK_REALTIME, 0, &sleep_time, NULL);
+		/*sleep(duration);*/
 
-	/* close driver */
-	for(i = 0; i < repeat; i++) {
-		if(0 <= openptr[i]) {
-			close(openptr[i]);	
+		/* close driver */
+		printf("Try to close driver...\n");
+		if(fd >= 0) {
+			if(close(fd) >= 0) {
+				printf("Driver closed!\n");
+			} else {
+				printf("Driver not closed -.-.-.-.- !\n");
+			}
 		}
 	}
-
+	printf("END THREAD\n");
 	pthread_exit(NULL);
-
 }
 
