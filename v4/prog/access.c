2c3774d523c4c7f4491f366d3a49e073e2c3543a
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-12-06 11:59:10 +0100

    refactor access

--- v4/prog/access.c
+++ v4/prog/access.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -15,18 +16,19 @@
 #define DEFAULT_REPEAT 5;
 #define NANO_TO_MILI 1000
 
-void *open_driver(void *thread_info);
+void *open_driver(void *threadarg);
 void help(void);
 
-
-
-struct thread_info
-{
+struct globalOptions {
 	struct timespec sleep_time;
 	int repeat;
 	int duration;
-	int threadNumber;
 	char *device;
+};
+
+struct thread_info {
+	struct globalOptions *global;
+	int threadNumber;
 };
 
 
@@ -38,22 +40,19 @@ int main(int argc, char *argv[])
 	int opentest, closetest = FALSE;
 	char *minorOneDevice;
 	int minortest=FALSE;
+	int duration = DEFAULT_DURATION;
+	int repeat = DEFAULT_REPEAT;
+	char *device;
 	
-	struct thread_info *thread_struct = NULL;
-	
-	thread_struct = malloc(sizeof (struct thread_info));
-	if (thread_struct == NULL){
-		fprintf(stderr, "Failed to allocate memory!\n");
-		return(-1);
-	}
-	
-	thread_struct->duration = DEFAULT_DURATION;
-	thread_struct->repeat = DEFAULT_REPEAT;
-	
-	/* threads */
-	pthread_t *threads;
+	struct globalOptions *global;
+    global = malloc (sizeof (struct globalOptions));
+    if (global == NULL) {
+                perror ("malloc");
+                exit (EXIT_FAILURE);
+    }
 
-	while(-1 != (opt = getopt (argc, argv, "d:oct:r:n:m:"))) {
+
+	while(-1 != (opt = getopt (argc, argv, "d:oct:r:n:m:h"))) {
 		switch(opt){
 			case 'o':
 				opentest = TRUE;
@@ -63,7 +62,7 @@ int main(int argc, char *argv[])
 				break;
 			case 't':
 				printf("waiting time: %s\n", optarg);
-				thread_struct->duration = atoi(optarg);
+				global->duration = atoi(optarg);
 				break;
 			case 'n':
 				printf("number of threads: %s\n", optarg);
@@ -71,30 +70,49 @@ int main(int argc, char *argv[])
 				break;
 			case 'd':
 				printf("path to device: %s\n", optarg);
-				thread_struct->device = optarg;
+				global->device = optarg;
 				break;
 			case 'r':
 				printf("number of repeats: %s\n", optarg);
-				thread_struct->repeat = atoi(optarg);
+				global->repeat = atoi(optarg);
 				break;
 			case 'm':
 				printf("multi-testing for minor number with device: %s\n", optarg);
 				minorOneDevice = optarg;
 				minortest = TRUE;
 				break;
-				
+			case 'h':
+				help();
+				break;
 		}
 	}
 
 	/* check if path ommitted */
-	if(thread_struct->device == NULL) {
-		printf("Usage: ./access -d [DEVICE_PATH] [OPTIONS]\n");
+	if(global->device == NULL) {
+		help();
 		return EXIT_FAILURE;
 	}	
 	
+		
+	/********************************************
+    *       struct initialisiation              *
+    *********************************************/
+
+	struct thread_info *thread_struct = NULL;	
+	thread_struct = malloc(numberOfThreads * sizeof (struct thread_info));
+		
+	/* threads */
+	pthread_t *threads;
+	pthread_attr_t *attr;
+	
 	threads = malloc(numberOfThreads * sizeof(pthread_t));
-	if (threads == NULL) {
-		return EXIT_FAILURE;
+	attr = malloc(numberOfThreads * sizeof(pthread_attr_t));
+	
+	/* check memory allocation */
+	if (thread_struct == NULL || attr == NULL || threads == NULL){
+		fprintf(stderr, "Failed to allocate memory!\n");
+		perror ("malloc");
+        exit (EXIT_FAILURE);
 	}
 
 	/* open test */
@@ -102,19 +120,35 @@ int main(int argc, char *argv[])
 	if(opentest) {
 		printf("OpenTest:\n");
 		for(i = 0; i < numberOfThreads; i++) {
+			thread_struct[i].global = global;
+			thread_struct[i].threadNumber = i;
+			
 			/* if minortest is active
 			 * 	-> every 2nd time use another minor number */
 			if(minortest && (i%2)) {
-				thread_struct->device = minorOneDevice;
+				thread_struct[i].global->device = minorOneDevice;
 			}
-			thread_struct->threadNumber= i;
+			//thread_struct->threadNumber= i;
+			if (pthread_attr_init (&attr[i]) == -1)
+            {
+				perror ("error in pthread_attr_init");
+            }
+            
 			printf("Start Thread %d\n", i);
-			pthread_create(&threads[i], NULL, open_driver, thread_struct);
+			pthread_create(&threads[i], &attr[i], open_driver, (void *) &thread_struct[i]);
+			printf("Thread %d created\n", i);
 		}	
 
+		printf("Alles losgeschickt\n");
 		for(i = 0; i < numberOfThreads; i++) {
 			printf("Join Thread %d\n", i);
 			pthread_join(threads[i], NULL);
+			
+			if (pthread_attr_destroy (&attr[i]) == -1)
+			{
+				perror ("error in pthread_attr_init");
+				exit (1);
+			}
 		}
 	}
 	
@@ -124,38 +158,43 @@ int main(int argc, char *argv[])
 	}
 	
 	free(threads);
+	free(attr);
+	free(thread_struct);
+	free(global);
+	
 	printf("End program\n");
 	
 	return 0;
 }
 
-void *open_driver(void *thread_info)
+void *open_driver(void *threadarg)
 {
 	int i, fd;
-	struct thread_info *t = (struct thread_info *) thread_info;
+	struct thread_info *t = (struct thread_info *) threadarg;
 	struct timespec sleep_time;
-	int duration = t->duration;
-	int repeat = t->repeat;
+	int duration = t->global->duration;
+	int repeat = t->global->repeat;
 	int threadNumber = t->threadNumber;
-
-	/* init time struct */
-	sleep_time.tv_sec = NANO_TO_MILI * duration;
 	
+	printf("Device: %s", t->global->device);
+
+	sleep_time.tv_sec = NANO_TO_MILI * duration;
+
 	for(i = 0; i < repeat; i++ ){
 		printf("THREAD %d: repeat %d\n", threadNumber, i );
 
 		printf("THREAD %d: Try to open Driver...\n", threadNumber);
-		fd = open(t->device, O_RDONLY);
+		fd = open(t->global->device, O_RDONLY);
 		if (fd < 0) {
 			printf("THREAD %d: Could not open", threadNumber);
 		} else {
 			printf("THREAD %d: Open Driver!\n", threadNumber);
 		}
 		
-		printf("THREAD %d: Sleep %d Millisecond\n", threadNumber, duration);
+		printf("THREAD %d: Sleep %d Millisecond\n", threadNumber, duration *NANO_TO_MILI);
 		clock_nanosleep(CLOCK_REALTIME, 0, &sleep_time, NULL);
 
-		/* close driver */
+		printf("weiter gehts");
 		printf("THREAD %d: Try to close driver...\n", threadNumber);
 		if(fd >= 0) {
 			if(close(fd) >= 0) {
