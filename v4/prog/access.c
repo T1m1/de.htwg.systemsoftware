9450b471fe9aad8c2e10a287026a9186a48ee7de
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-12-07 12:12:33 +0100

    implement write test

--- v4/prog/access.c
+++ v4/prog/access.c
@@ -12,10 +12,11 @@
 #define TRUE 1
 #define FALSE 0
 
-#define DEFAULT_NUMBER_OF_THREADS 5;
-#define DEFAULT_DURATION 1;
-#define DEFAULT_REPEAT 5;
+#define DEFAULT_NUMBER_OF_THREADS 5
+#define DEFAULT_DURATION 1000
+#define DEFAULT_REPEAT 5
 #define NANO_TO_MILI 1000000
+#define BUFFER_SIZE 128
 
 void *open_driver(void *threadarg);
 void help(void);
@@ -26,6 +27,11 @@ struct globalOptions {
 	int repeat;
 	int duration;
 	int verbose;
+	int open;
+	int close;
+	int read;
+	int write;
+	char *writeFile;
 };
 
 struct thread_info {
@@ -40,7 +46,6 @@ int main(int argc, char *argv[])
 	/* parse options */
 	int opt;
 	int numberOfThreads = DEFAULT_NUMBER_OF_THREADS;
-	int opentest, closetest = FALSE;
 	char *minorOneDevice;
 	int minortest = FALSE;
 	char *device = NULL;
@@ -55,13 +60,20 @@ int main(int argc, char *argv[])
     global->repeat = DEFAULT_REPEAT;
 
 
-	while(-1 != (opt = getopt (argc, argv, "d:oct:r:n:m:hv"))) {
+	while(-1 != (opt = getopt (argc, argv, "d:oct:e:n:m:hvrw:"))) {
 		switch(opt){
 			case 'o':
-				opentest = TRUE;
+				global->open = TRUE;
 				break;
 			case 'c':
-				closetest = TRUE;
+				global->close = TRUE;
+				break;
+			case 'r':
+				global->read = TRUE;
+				break;
+			case 'w':
+				global->write = TRUE;
+				global->writeFile = optarg;
 				break;
 			case 't':
 				global->duration = atoi(optarg);
@@ -72,7 +84,8 @@ int main(int argc, char *argv[])
 			case 'd':
 				device = optarg;
 				break;
-			case 'r':
+			case 'e':
+				/* echo instead of repead, r=read */
 				global->repeat = atoi(optarg);
 				break;
 			case 'm':
@@ -101,8 +114,7 @@ int main(int argc, char *argv[])
     verbPrintf(global->verbose, "Number of threads: %d\n", numberOfThreads);
     verbPrintf(global->verbose, "Path to device: %s\n", device);
     verbPrintf(global->verbose, "Number of repeats: %d\n", global->repeat);
-    verbPrintf(global->verbose, "Multi-Testing for minor number is ON! With device: %s\n", minorOneDevice);	
-		
+    	
 	/********************************************
     *       struct initialisiation              *
     *********************************************/
@@ -126,48 +138,41 @@ int main(int argc, char *argv[])
 
 	/* open test */
 	int i;
-	if(opentest) {
-		verbPrintf(global->verbose, "Start opentest:\n");
-		for(i = 0; i < numberOfThreads; i++) {
-			thread_struct[i].global = global;
-			thread_struct[i].threadNumber = i;
-			
-			/* if minortest is active
-			 * 	-> every 2nd time use another minor number */
-			if(minortest && (i%2)) {
-				verbPrintf(global->verbose, "Thread nr. %d start with device %s\n", i, minorOneDevice);
-				thread_struct[i].device = minorOneDevice;
-			} else {
-				verbPrintf(global->verbose, "Thread nr. %d start with device %s\n", i, device);
-				thread_struct[i].device = device;	
-			}
-			
-			if (pthread_attr_init (&attr[i]) == -1)
-            {
-				perror ("error in pthread_attr_init");
-            }
-            
-			pthread_create(&threads[i], &attr[i], open_driver, (void *) &thread_struct[i]);
-		}	
-
-		for(i = 0; i < numberOfThreads; i++) {
-			verbPrintf(global->verbose, "Join thread nr. %d\n", i);
-			pthread_join(threads[i], NULL);
-			
-			if (pthread_attr_destroy (&attr[i]) == -1)
-			{
-				perror ("error in pthread_attr_init");
-				exit (1);
-			}
+	verbPrintf(global->verbose, "Start tests:\n");
+	for(i = 0; i < numberOfThreads; i++) {
+		thread_struct[i].global = global;
+		thread_struct[i].threadNumber = i;
+		
+		/* if minortest is active
+		 * 	-> every 2nd time use another minor number */
+		if(minortest && (i%2)) {
+			verbPrintf(global->verbose, "Thread nr. %d start with device %s\n", i, minorOneDevice);
+			thread_struct[i].device = minorOneDevice;
+		} else {
+			verbPrintf(global->verbose, "Thread nr. %d start with device %s\n", i, device);
+			thread_struct[i].device = device;	
 		}
-		verbPrintf(global->verbose, "Finish opentest!\n");
+		
+		if (pthread_attr_init (&attr[i]) == -1)
+		{
+			perror ("error in pthread_attr_init");
+		}
+		
+		pthread_create(&threads[i], &attr[i], open_driver, (void *) &thread_struct[i]);
+	}	
+
+	for(i = 0; i < numberOfThreads; i++) {
+		verbPrintf(global->verbose, "Join thread nr. %d\n", i);
+		pthread_join(threads[i], NULL);
+		
+		if (pthread_attr_destroy (&attr[i]) == -1)
+		{
+			perror ("error in pthread_attr_init");
+			exit (1);
+		}
 	}
+	verbPrintf(global->verbose, "Finish tests!\n");
 	
-	/* close test */
-	if(closetest) {
-		verbPrintf(global->verbose, "Start closetest:\n");
-		verbPrintf(global->verbose, "Finish closetest!\n");
-	}
 	
 	free(threads);
 	free(attr);
@@ -193,30 +198,75 @@ void *open_driver(void *threadarg)
 	
 	for(i = 0; i < repeat; i++ ){
 		verbPrintf(t->global->verbose, "Thread %d: repeat %d\n", threadNumber, i);
-		verbPrintf(t->global->verbose, "Thread %d: try to open driver..\n", threadNumber);
-		fd = open(t->device, O_RDONLY);
-		if (fd < 0) {
-			verbPrintf(t->global->verbose, "Thread %d: could not open driver!\n", threadNumber);
-		} else {
-			verbPrintf(t->global->verbose, "Thread %d: open driver!\n", threadNumber);
+		if(t->global->open || t->global->close | t->global->read) {	
+			verbPrintf(t->global->verbose, "Thread %d: try to open driver..\n", threadNumber);
+			fd = open(t->device, O_RDONLY);
+			if (fd < 0) {
+				verbPrintf(t->global->verbose, "Thread %d: could not open driver!\n", threadNumber);
+			} else {
+				verbPrintf(t->global->verbose, "Thread %d: open driver!\n", threadNumber);
+			}
 		}
 		
+		/* read test */
+		if (t->global->read) {
+			char buf[BUFFER_SIZE];
+			int ret;
+			int rounds = 0;
+			
+			while ((rounds < 50) && (ret = read(fd, buf, BUFFER_SIZE))) {
+				verbPrintf(t->global->verbose, "Read data %s\n", buf);
+				rounds++;
+			}
+
+			if (!ret)
+				printf("Reached EOF\n");
+			else {
+				perror("Error closing file.\n");
+				pthread_exit(NULL);
+			}
+		}
+		
+		/* write test */
+		if (t->global->write) {
+			ssize_t nrd;
+			char buf[BUFFER_SIZE];
+			
+			int wf_fd = open(t->global->writeFile, O_RDONLY);
+			int w_fd = open(t->device, O_WRONLY);
+
+			if ((w_fd < 0) || (wf_fd < 0)) {
+				verbPrintf(t->global->verbose, "Thread %d: could not open driver for writing or file for reading!\n", threadNumber);
+			} else {
+				verbPrintf(t->global->verbose, "Thread %d: open driver for writing!\n", threadNumber);
+				
+				while((nrd = read(wf_fd, buf, 50))){
+					 write(w_fd, buf, nrd);
+					 verbPrintf(t->global->verbose, "Write data %s\n", buf);
+				}
+				close(wf_fd);
+				close(w_fd);
+			}
+		}
+
 		verbPrintf(t->global->verbose, "Thread %d: sleep %d miliseconds!\n", threadNumber, duration);
 		clock_nanosleep(CLOCK_REALTIME, 0, &sleep_time, NULL);
 
-
-		verbPrintf(t->global->verbose, "Thread %d: try to close driver!\n", threadNumber);		
-		if(fd >= 0) {
-			if(close(fd) >= 0) {
-				verbPrintf(t->global->verbose, "Thread %d: driver closed!\n", threadNumber);
-			} else {
-				verbPrintf(t->global->verbose, "Thread %d: driver not closed!\n", threadNumber);
+		if(t->global->open || t->global->close | t->global->read) {	
+			verbPrintf(t->global->verbose, "Thread %d: try to close driver!\n", threadNumber);		
+			if(fd >= 0) {
+				if(close(fd) >= 0) {
+					verbPrintf(t->global->verbose, "Thread %d: driver closed!\n", threadNumber);
+				} else {
+					verbPrintf(t->global->verbose, "Thread %d: driver not closed!\n", threadNumber);
+				}
 			}
 		}
 	}
 	verbPrintf(t->global->verbose, "Thread %d: end of thread!\n", threadNumber);
 	pthread_exit(NULL);
 }
+
 
 void help(void)
 {
@@ -227,11 +277,13 @@ void help(void)
 			"\t-d			path to device"
 			"\t-o			open test\n"
 			"\t-c			close test\n"
+			"\t-r			read test\n"
+			"\t-w [PATH]	write test - PATH to any file to read\n"
 			"\t-t [TIME]	time in millisecond to sleep between open and close\n"
 			"\t				default: 1000\n"
 			"\t-n [NR]		number of threads\n"
 			"\t				default: 5\n"
-			"\t-r [NR]		number of repeats\n"
+			"\t-e [NR]		number of echos (repeats)\n"
 			"\t				default: 5\n"
 			"\t-m [PATH]	start tests with different minor numbers\n"
 			"\t	  [PATH]	to device with different minor\n"
