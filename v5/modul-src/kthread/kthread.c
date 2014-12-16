039372b5a485d19eb56e4fc7a893df1f5041ba96
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-12-16 10:35:20 +0100

    add initial version of ktrhead

---
+++ v5/modul-src/kthread/kthread.c
@@ -0,0 +1,78 @@
+#include <linux/init.h>
+#include <linux/module.h>
+/* prototypes */
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+
+#define DRIVER_NAME "kthread"
+
+/* normaly in stdlib.h */
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+static int major;
+static struct file_operations fobs;
+static dev_t dev_number;
+static struct cdev *driver_object;
+struct class *template_class;
+
+static int __init ModInit(void)
+{
+	/* reserve device driver number */
+	if(alloc_chrdev_region(&dev_number, 0, 1, DRIVER_NAME) < 0)
+	{
+		printk("failed to alloc_chrdev_region\n");	
+		return -EIO;
+	}
+	/* reserve object */
+	driver_object = cdev_alloc();
+	
+	if(driver_object == NULL)
+	{
+		goto free_device_number;
+	}
+	driver_object->owner = THIS_MODULE;
+	driver_object->ops = &fobs;
+	
+	if(cdev_add(driver_object, dev_number, 1))
+	{
+		goto free_cdev;
+	}
+	
+	template_class = class_create(THIS_MODULE, DRIVER_NAME);
+	device_create(template_class, NULL, dev_number, NULL, "%s", DRIVER_NAME);
+	
+	major = MAJOR(dev_number);
+	
+	printk("Major number: %d\n", major);
+	return EXIT_SUCCESS;
+	
+free_cdev:
+	kobject_put(&driver_object->kobj);
+	
+free_device_number:
+	unregister_chrdev_region(dev_number, 1);
+	return -EIO;
+	
+}
+
+static void __exit ModExit(void) 
+{
+	device_destroy(template_class, dev_number);
+	class_destroy(template_class);
+	cdev_del(driver_object);
+	unregister_chrdev_region(dev_number, 1);
+	return;
+}
+
+module_init(ModInit);
+module_exit(ModExit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Timotheus Ruprecht and  Steffen Gorenflo");
+MODULE_DESCRIPTION("Modul Template");
+MODULE_SUPPORTED_DEVICE("none");
+
+
