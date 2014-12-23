3d6c2c08852edf97998edd4d849ca1b2801f9241
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-12-23 18:43:45 +0100

    add access to modul config

---
+++ v5/modul-package/access/access.mk
@@ -0,0 +1,26 @@
+##### TEMPLATE
+
+ACCESS_VERSION = 1.0
+ACCESS_SITE_METHOD = file
+ACCESS_SITE = access-$(ACCESS_VERSION).tar.gz
+
+
+ACCESS_DEPENDENCIES = linux
+
+define ACCESS_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+endef
+
+define ACCESS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/access $(TARGET_DIR)/usr/bin/access
+endef
+
+define ACCESS_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+define ACCESS_UNINSTALL_TARGET_CMS
+	rm $(TARGET_DIR)/usr/bin/access
+endef
+
+$(eval $(generic-package))
