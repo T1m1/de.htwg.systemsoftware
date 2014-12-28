55b4e81ac8bde8656c0a94f5fc7fe7f68c1af91a
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-12-28 10:57:26 +0100

    add package for buf

---
+++ v5/modul-package/buf/buf.mk
@@ -0,0 +1,27 @@
+##### BUF
+
+BUF_VERSION = 1.0
+BUF_SITE_METHOD = file
+BUF_SITE = buf-$(BUF_VERSION).tar.gz
+
+
+BUF_DEPENDENCIES = linux
+
+define BUF_BUILD_CMDS
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules 
+endef
+
+define BUF_INSTALL_TARGET_CMDS
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
+	$(INSTALL) -m 0755 -D $(@D)/test_template.sh $(TARGET_DIR)/usr/bin/test_template.sh
+endef
+
+define BUF_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+define BUF_UNINSTALL_TARGET_CMS
+	rm $(TARGET_DIR)/usr/bin/template
+endef
+
+$(eval $(generic-package))
