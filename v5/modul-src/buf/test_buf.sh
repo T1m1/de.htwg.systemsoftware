4a21d626c8ac4e39afe78ea11d4790e803957933
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2014-12-28 10:55:53 +0100

    init buf module

---
+++ v5/modul-src/buf/test_buf.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+PROG_NAME=buf
+PATH_TO_KO=/lib/modules/3.17.2/extra
+
+## init
+# unload modul (if loaded)
+rmmod $PROG_NAME > /dev/null
+# clear dmesg output 
+dmesg -c > /dev/null
+
+# show modul informations
+echo "********** modul info **********"
+modinfo $PATH_TO_KO/$PROG_NAME.ko
+
+# load modul
+insmod $PATH_TO_KO/$PROG_NAME.ko
+
+# show kernel logs for loading (e.g. last 10 lines)
+echo "********** kernel logs for loading **********"
+dmesg -c
+
+# show information from /proc/devices
+echo "********** /proc/devices **********"
+cat /proc/devices
+
+# unload modul (without .ko)
+rmmod $PROG_NAME
+
+# show kernel logs for unload
+echo "********** kernel logs for unloading **********"
+dmesg -c
+
