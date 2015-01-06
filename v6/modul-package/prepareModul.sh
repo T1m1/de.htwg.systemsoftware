a35fe29def3dc08f3e3e0c80d720ea96a848b39e
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2015-01-06 08:53:55 +0100

    init modul package

---
+++ v6/modul-package/prepareModul.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BUILDROOT_PACKAGE=../../buildroot/package
+MODUL_PACKAGE=../../v6/modul-package
+PACKAGE=syso
+
+cd $BUILDROOT_PACKAGE
+# make symbolic link to package directory
+ln -s $MODUL_PACKAGE syso
+
+
+sed -i -r '/	source \"package\/sound-theme-freedesktop\/Config.in\"/a\\x9source "package/syso/Config.in"' Config.in
---
+++ v6/modul-package/template/template.mk
@@ -0,0 +1,27 @@
+##### TEMPLATE
+
+TEMPLATE_VERSION = 1.0
+TEMPLATE_SITE_METHOD = file
+TEMPLATE_SITE = template-$(TEMPLATE_VERSION).tar.gz
+
+
+TEMPLATE_DEPENDENCIES = linux
+
+define TEMPLATE_BUILD_CMDS
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules 
+endef
+
+define TEMPLATE_INSTALL_TARGET_CMDS
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
+	$(INSTALL) -m 0755 -D $(@D)/test_template.sh $(TARGET_DIR)/usr/bin/test_template.sh
+endef
+
+define TEMPLATE_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+define TEMPLATE_UNINSTALL_TARGET_CMS
+	rm $(TARGET_DIR)/usr/bin/template
+endef
+
+$(eval $(generic-package))
