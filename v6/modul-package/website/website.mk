dfe0a272eb2e32bd2510d1910b75f5e8e7a3885e
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2015-01-11 10:38:25 +0100

    add package for website

---
+++ v6/modul-package/website/website.mk
@@ -0,0 +1,20 @@
+##### WEBSITE
+
+WEBSITE_VERSION = 1.0
+WEBSITE_SITE_METHOD = file
+WEBSITE_SITE = website-$(WEBSITE_VERSION).tar.gz
+
+
+WEBSITE_DEPENDENCIES = linux
+
+define WEBSITE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/index.html $(TARGET_DIR)/www/index.html
+	$(INSTALL) -m 0755 -D $(@D)/main.js $(TARGET_DIR)/www/js/main.js
+	$(INSTALL) -m 0755 -D $(@D)/led250.sh $(TARGET_DIR)/www/cgi-bin/led250.sh
+	$(INSTALL) -m 0755 -D $(@D)/ledflip.sh $(TARGET_DIR)/www/cgi-bin/ledflip.sh
+
+
+endef
+
+
+$(eval $(generic-package))
