081ae9555acd21d0282f080fb0bbde707ee7fc2b
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2015-01-12 16:00:13 +0100

    driver_write implemented

--- v5/modul-src/buf/buf.c
+++ v5/modul-src/buf/buf.c
@@ -9,12 +9,15 @@
 #include <linux/wait.h>
 #include <asm/signal.h>
 #include <linux/sched.h>
+#include <asm/uaccess.h>
 
 #define DRIVER_NAME "buf"
 
 /* normaly in stdlib.h */
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+
+#define BUF_SIZE 32
 
 static int major;
 static dev_t dev_number;
@@ -38,10 +41,12 @@ static struct file_operations fobs =
 	.release = driver_close,
 };
 
-atomic_t bytes_available;
+atomic_t bytes_available = 0;
+atomic_t bytes_that_can_be_written = BUF_SIZE;
 
-/* define makro for atomic read */
-#define READ_POSSIBLE (atomic_read(&bytes_available)!=0)
+/* define makro for atomic read and write*/
+#define READ_POSSIBLE (atomic_read(&bytes_available) != 0)
+#define WRITE_POSSIBLE (atomic_read(&bytes_that_can_be_written)!=0)
 
 
 
@@ -58,17 +63,15 @@ static int driver_close(struct inode *geraetedatei, struct file *instanz) {
 static ssize_t driver_read(struct file *instanz, char __user *userbuffer, size_t count, loff_t *offset) {
 	
 	int not_copied, to_copy, retval;
-	/*TODO: initialize "data_available"*/
 
 	/* non blocking mode and no data available */
 	if (!READ_POSSIBLE && (instanz->f_flags & O_NONBLOCK)) {
 		return -EAGAIN;
 	}
 	
-	/* blocking mode*/
-		/* check available data and maybe wait */
+	/* blocking mode:  check available data and maybe wait */
 	retval = wait_event_interruptible(wait_queue_for_read, READ_POSSIBLE);	
-	
+
 	if (retval == -ERESTARTSYS) {
 		printk("Failure reading data: Interrupt by signal\n");
 		return -ERESTARTSYS;
@@ -77,13 +80,13 @@ static ssize_t driver_read(struct file *instanz, char __user *userbuffer, size_t
 		printk("Failure reading data\n");
 		return -EIO;
 	}
-	
 		
-	to_copy = min((size_t) atomic_read(&bytes_available), count);
-	not_copied = copy_to_user(userbuffer, /*TODO: driver buffer*/, to_copy);
+	to_copy = min((size_t) atomic_read(&bytes_that_can_be_written), count);
+	not_copied = copy_from_user(/*TODO: driver buffer*/, userbuffer,to_copy);
 
-	/* TODO: reduce buffer*/
-	atomic_sub(to_copy-not_copied, &bytes_available);
+	/* TODO: write bytes to buffer*/
+	atomic_sub(to_copy-not_copied, &bytes_that_can_be_written);
+	atomic_add(to_copy-not_copied, &bytes_available);
 	     
 	/* return to user application */
     return to_copy-not_copied;
@@ -92,6 +95,35 @@ static ssize_t driver_read(struct file *instanz, char __user *userbuffer, size_t
 
 static ssize_t driver_write(struct file *instanz, const char __user *userbuffer, size_t count, loff_t *offs) {
 	
+	int not_copied, to_copy, retval;
+	
+	/* non blocking mode and no space to write */
+	if (!WRITE_POSSIBLE && (instanz->f_flags & O_NONBLOCK)) {
+		return -EAGAIN;
+	}
+	
+	/* blocking mode: check space to write data and maybe wait */
+	retval = wait_event_interruptible(wait_queue_for_write, WRITE_POSSIBLE);	
+
+	if (retval == -ERESTARTSYS) {
+		printk("Failure writing data: Interrupt by signal\n");
+		return -ERESTARTSYS;
+	}
+	if (retval != 0) {
+		printk("Failure writing data\n");
+		return -EIO;
+	}
+	
+	to_copy = min((size_t) atomic_read(&bytes_available), count);
+	not_copied = copy_to_user(userbuffer, /*TODO: driver buffer*/, to_copy);
+
+	/* TODO: delete bytes from buffer*/
+	atomic_sub(to_copy-not_copied, &bytes_available);
+	atomic_add(to_copy-not_copied, &bytes_that_can_be_written);
+	     
+	/* return to user application */
+    return to_copy-not_copied;
+
 }
 
 static int __init ModInit(void)
@@ -119,14 +151,18 @@ static int __init ModInit(void)
 	
 	buf_class = class_create(THIS_MODULE, DRIVER_NAME);
 	device_create(buf_class, NULL, dev_number, NULL, "%s", DRIVER_NAME);
+
+	
+	major = MAJOR(dev_number);
+	printk("Major number: %d\n", major);
+	
+		
+	/*TODO: initialize "data_available"*/
 	
 	/* init queues */
 	init_waitqueue_head( &wait_queue_for_read );
     init_waitqueue_head( &wait_queue_for_write );
 	
-	major = MAJOR(dev_number);
-	
-	printk("Major number: %d\n", major);
 	return EXIT_SUCCESS;
 	
 free_cdev:
