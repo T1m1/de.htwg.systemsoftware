40c609cc81984a24f76e7175b347d7454c7f6be8
Timotheus.Ruprecht <tiruprec@htwg-konstanz.de>
2015-01-12 15:24:56 +0100

    adjust makefiles for webpage

--- v6/modul-package/led250/led250.mk
+++ v6/modul-package/led250/led250.mk
@@ -13,16 +13,8 @@ endef
 
 define LED250_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/led250 $(TARGET_DIR)/usr/bin/led250
-	$(INSTALL) -m 0755 -D $(@D)/test_led250.sh $(TARGET_DIR)/usr/bin/test_led250.sh
+	$(INSTALL) -m 0755 -D $(@D)/test_led250.sh $(TARGET_DIR)/usr/bin/test_ledrpi250.sh
 	
-endef
-
-define LED250_CLEAN_CMDS
-	$(MAKE) -C $(@D) clean
-endef
-
-define LED250_UNINSTALL_TARGET_CMS
-	rm $(TARGET_DIR)/usr/bin/access
 endef
 
 $(eval $(generic-package))
