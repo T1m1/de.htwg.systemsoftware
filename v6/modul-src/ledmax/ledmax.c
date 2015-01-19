7860ced1c659eaaab8659f6d154663dc24c2032d
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2015-01-19 15:50:24 +0100

    improved test of frequency incl sysfs

--- v6/modul-src/ledmax/ledmax.c
+++ v6/modul-src/ledmax/ledmax.c
@@ -8,10 +8,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include "gpio.h"
 
 
-/* blink thousand times */
-#define MAX_BLINK            		1000
+/* blink million times */
+#define MAX_BLINK            		1000000
+#define LED 18
+#define LOOP_TIMES 5
 
 
 #define LED_ON                      "0"
@@ -20,14 +23,15 @@
 
 
 void check(int err);
-ulong calculate(struct timespec time_before, struct timespec time_after);
+float calculate(struct timespec time_before, struct timespec time_after);
+void blink(int file);
 
 
 
 int main(void)
 {
-	int file, i;
-	ulong hz;
+	int file, led_fd, i;
+	float hz;
 	int err;
 	struct timespec time_before, time_after;
 	clockid_t clk_id;
@@ -58,10 +62,97 @@ int main(void)
          exit(EXIT_FAILURE);
     }
     
-	printf("Let the LED blink thousand times\n");
+	printf("Let the LED blink a million times (5 times)\n");
 	
-	err = clock_gettime(clk_id, &time_before);
-	check(err);
+	hz = 0;
+	for (i = 0; i < LOOP_TIMES; i++) 
+	{
+		
+		err = clock_gettime(clk_id, &time_before);
+		check(err);
+		
+		blink(file);
+		
+		err = clock_gettime(clk_id, &time_after);
+		check(err);
+		
+		hz = hz + calculate(time_before, time_after);
+	}
+	
+	printf("Done blinking\n");
+	
+	hz = hz / LOOP_TIMES;
+	
+	printf("Average frequenzy for driver is: %.0f Hz\n", hz);
+
+    /* close driver */
+    if (0 != close(file)) {
+        printf("Closing driver failed!\n");
+    }
+    
+    printf("Testing now sysfs\n");
+    
+    gpio_export(LED);
+	gpio_set_dir(LED, 1);
+	led_fd = gpio_fd_open_write(LED);
+	
+	printf("Blink LED through sysfile\n");
+	hz = 0;
+	for (i = 0; i < LOOP_TIMES; i++) 
+	{
+		err = clock_gettime(clk_id, &time_before);
+		check(err);
+		
+		blink(led_fd);
+		
+		err = clock_gettime(clk_id, &time_after);
+		check(err);
+		
+		hz = hz + calculate(time_before, time_after);
+	}
+	printf("Done blinking\n");
+	
+	hz = hz / LOOP_TIMES;
+	
+	printf("Average frequenzy for sysfs is: %.0f Hz\n", hz);
+	
+    printf("End of Programm.\n");
+    return EXIT_SUCCESS;
+}
+
+
+void
+check(int err)
+{
+	if (err == -1) {
+		printf("Error while getting time\n");
+		exit(EXIT_FAILURE);
+	}
+}
+	
+float
+calculate(struct timespec time_before, struct timespec time_after) 
+{
+	time_t s;
+	time_t ns;
+	float res;
+	
+	s = time_after.tv_sec -time_before.tv_sec ;
+	ns = time_after.tv_nsec - time_before.tv_nsec ;
+	
+	res = ns / 10000000000.0;	
+	res = s + res;
+	res = MAX_BLINK / res;
+	
+	printf("Frequency: %.0f Hz\n", res);
+	return res;
+
+}
+
+void
+blink(int file) 
+{	
+	int i;
 	for(i = 0; i < MAX_BLINK; i++) 
 	{
 
@@ -80,58 +171,4 @@ int main(void)
         }
 
     }
-    err = clock_gettime(clk_id, &time_after);
-	check(err);
-	printf("Done blinking\n");
-	
-	/* led off */
-    if (1 != write(file, LED_OFF, 1)) {
-        printf("Switch off driver failed after loop!\n");
-    }
-	
-	hz = calculate(time_before, time_after);
-	
-	printf("Time before: %ld\n", time_before.tv_sec);
-	printf("Time after: %ld\n", time_after.tv_sec);
-	printf("The calculated max frequency is: %lu\n", hz);
-
-
-
-    /* close driver */
-    if (0 != close(file)) {
-        printf("Closing driver failed!\n");
-    }
-
-    printf("End of Programm.\n");
-    return EXIT_SUCCESS;
-}
-
-
-void
-check(int err)
-{
-	if (err == -1) {
-		printf("Error while getting time\n");
-		exit(EXIT_FAILURE);
-	}
-}
-	
-ulong
-calculate(struct timespec time_before, struct timespec time_after) 
-{
-	ulong s;
-	ulong ns;
-	ulong res;
-	
-	s = time_before.tv_sec - time_after.tv_sec;
-	ns = time_before.tv_nsec - time_after.tv_nsec;
-	
-	res = s + (ns / 1000000000.0);
-	
-	printf("Duration: %lu\n", res);
-	
-	res = MAX_BLINK / res;
-	
-	return res;
-
 }
